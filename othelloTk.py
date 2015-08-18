047f8757c41d462439fd15ab9faf237a39f8f163
John Cheetham <jcheetham19@gmail.com>
2015-08-18 19:04:37 +0100

    Force board aspect of 1:1

--- othelloTk.py
+++ othelloTk.py
@@ -69,7 +69,14 @@ class Othello(Frame):
         self.square_height = square_height
         board_width = (square_width * 8)
         board_height = board_width
-        self.canvas = Canvas(self, width=board_width, height=board_height, bg="darkgreen")
+
+        pad_frame = Frame(borderwidth=0, background="light blue", width=board_width, height=board_height)
+        #pad_frame.grid(row=0, column=0, sticky="nsew", padx=10, pady=20)
+        pad_frame.grid(row=0, column=0, sticky="nsew")
+
+        self.canvas = Canvas(root, width=board_width, height=board_height, bg="darkgreen")
+        set_aspect(self.canvas, pad_frame, aspect_ratio=1.0/1.0)
+
         self.canvas.bind("<Configure>", self.on_resize)
         self.canvas.grid(row=0, column=0, sticky=N + S + E + W)
         self.rowconfigure(0, weight=1)
@@ -426,6 +433,31 @@ class Othello(Frame):
                 return 0
             xx = xx + incx
             yy = yy + incy
+
+def set_aspect(content_frame, pad_frame, aspect_ratio):
+    # a function which places a frame within a containing frame, and
+    # then forces the inner frame to keep a specific aspect ratio
+
+    def enforce_aspect_ratio(event):
+        # when the pad window resizes, fit the content into it,
+        # either by fixing the width or the height and then
+        # adjusting the height or width based on the aspect ratio.
+
+        # start by using the width as the controlling dimension
+        desired_width = event.width
+        desired_height = int(event.width / aspect_ratio)
+
+        # if the window is too tall to fit, use the height as
+        # the controlling dimension
+        if desired_height > event.height:
+            desired_height = event.height
+            desired_width = int(event.height * aspect_ratio)
+
+        # place the window, giving it an explicit size
+        content_frame.place(in_=pad_frame, x=0, y=0, 
+            width=desired_width, height=desired_height)
+
+    pad_frame.bind("<Configure>", enforce_aspect_ratio)
 
 root = Tk()
 #root.resizable(width=FALSE, height=FALSE)
